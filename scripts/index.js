633c467f90df92d37900cf48ec08fb8624afdb28
mmariaiv <mari.ivashova@gmail.com>
2023-02-19 15:22:20 +0300

    +almostAllHtml_AllIndexCssHalfJs

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,8 @@ let submitForm = document.querySelector(".popup__submit-button");
 let formElement = document.querySelector(".popup");
 let nameInput = document.querySelector(".popup__name-input");
 let jobInput = document.querySelector(".popup__description-input");
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__bio");
 
 function openPopup() {
 	formElement.classList.add("popup_opened");
@@ -17,8 +19,6 @@ function handleFormSubmit(evt) {
 	evt.preventDefault();
 	let nameDefinition = nameInput.value;
 	let jobDefinition = jobInput.value;
-	let profileName = document.querySelector(".profile__name");
-	let profileDescription = document.querySelector(".profile__bio");
 
 	if (nameDefinition.replaceAll(" ", "") != "") {
 		profileName.textContent = `${nameDefinition}`;
@@ -40,17 +40,3 @@ closeForm.addEventListener("click", closePopup);
 editProfileButton.addEventListener("click", openPopup);
 
 let likeButtons = document.querySelectorAll(".element__like-button");
-function setLike(likeButton) {
-	likeButton = likeButton.target;
-	if (likeButton.classList.contains("element__like-button_disabled")) {
-		likeButton.classList.remove("element__like-button_disabled");
-		likeButton.classList.add("element__like-button_active");
-	} else {
-		likeButton.classList.remove("element__like-button_active");
-		likeButton.classList.add("element__like-button_disabled");
-	}
-}
-
-likeButtons.forEach((likeButton) => {
-	likeButton.addEventListener("click", setLike);
-});
